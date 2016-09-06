33173fe267d811cc96a0342d637352cf313077dc
dian <dian_t_d@yahoo.co.uk>
2016-09-06 08:58:12 -0700

    We should allow multiple users to have room authentication

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ WebRTC audio/video conferencing app using React.
 npm i
 npm i webpack -global
 apt-get install redis-server
-redis-server
+sudo service redis-server status redis-server is running
--- server.babel.js
+++ server.babel.js
@@ -27,81 +27,47 @@ app.use(express.static('public'));
 app.use((req, res) => res.sendFile(__dirname + '/public/index.html'));
 io.sockets.on('connection', socket => {
   let room = '';
+  const sessionID = socket.request.sessionID,
+    create = err => {
+      if (err) {
+        return console.log(err);
+      }
+      socket.join(room);
+      socket.emit('create');
+    };
   // sending to all clients in the room (channel) except sender
   socket.on('message', message => socket.broadcast.to(room).emit('message', message));
   socket.on('find', () => {
     const url = socket.request.headers.referer.split('/');
     room = url[url.length - 1];
-    console.log(110, room, socket.request.session.id)
-    let sr = io.sockets.adapter.rooms[room];
-    
-    client.lrange(room, 0, -1, function(err, reply) {
+    client.lrange(room, 0, -1, (err, reply) => {
+      if (err) {
+        return console.log(err);
+      }
       const host = 'sess:' + reply[0];
-      console.log('reply', reply);
       if (reply.length === 0) {
-        client.rpush([room, socket.request.session.id], function(err, reply) {
-          console.log('reply1', err, reply);
-          socket.join(room);
-          socket.emit('create');
-        });
+        client.rpush([room, sessionID], create);
       }
       else if (reply.length === 1) {
-
-        client.exists(host, function(err, reply) {
-          console.log('reply2', host, reply)
-          if (reply === 1) {
-              console.log('exists and join');
-              // a room with a host is found
-              client.rpush([room, socket.request.session.id], function(err, reply) {
-                console.log('reply1', err, reply);
-                socket.join(room);
-                socket.emit('join');
-              });
-          } else {
-            console.log('doesn\'t exist');
-            client.lset([room, 0, socket.request.session.id], function(err, reply) {
-              console.log('reply4', err, reply);
-              socket.join(room);
-              socket.emit('create');
-            });
+        client.exists(host, (err, reply) => {
+          if (err) {
+            return console.log(err);
           }
+          // a room with a host is found
+          reply === 1 ? socket.emit('join') : client.lset([room, 0, sessionID], create);
         });
       }
       else if (reply.length === 2) {
-        client.exists(host, function(err, reply) {
-          if (reply === 1) {
-              console.log('exists and full');
-              // max two clients
-              socket.emit('full');
-          } else {
-            client
-              .multi()
-              .del(room)
-              .client.rpush([room, socket.request.session.id])
-              .exec(function(err, reply) {
-                console.log('reply3', err, reply);
-                socket.join(room);
-                socket.emit('create');
-              })
+        client.exists(host, (err, reply) => {
+          if (err) {
+            return console.log(err);
           }
+          // max two clients
+          reply === 1 ? socket.emit('full') :
+            client.multi().del(room).rpush([room, sessionID]).exec(create);
         });
       }
     });
-    /*if (sr === undefined) {
-      // no room with such name is found so create it
-      socket.join(room);
-      socket.emit('create');
-      //socket.request.session.room = room;
-      //socket.request.session.save();
-      console.log(111, room, socket.request.session.save)
-    } else if (sr.length === 1) {
-      console.log(11, socket.id, sr)
-      // a room with a host is found
-      socket.emit('join');
-    } else {
-      // max two clients
-      socket.emit('full');
-    }*/
   });
   socket.on('auth', data => {
     data.sid = socket.id;
@@ -109,9 +75,15 @@ io.sockets.on('connection', socket => {
     socket.broadcast.to(room).emit('approve', data);
   });
   socket.on('accept', id => {
-    io.sockets.connected[id].join(room);
-    // sending to all clients in 'game' room(channel), include sender
-    io.in(room).emit('bridge');
+    const so = io.sockets.connected[id];
+    client.rpush([room, so.request.sessionID], (err, reply) => {
+      if (err) {
+        return console.log(err);
+      }
+      so.join(room);
+      // sending to all clients in 'game' room(channel), include sender
+      io.in(room).emit('bridge');
+    });
   });
   socket.on('reject', () =>socket.emit('full'));
   socket.on('bye', () => {
