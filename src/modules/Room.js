ea798439af1a1fd3c0e373b8bb88cfa2fd35f35f
dian <dian_t_d@yahoo.co.uk>
2016-09-11 03:59:45 -0700

    Set class like structure

--- src/modules/Room.js
+++ src/modules/Room.js
@@ -6,34 +6,14 @@ class Room extends React.Component {
   constructor(props) {
     super(props);
   }
-  state = {
-      user: '',
-      audio: false,
-      video: true,
-      full: false
-  }
-  haveMedia = false;
-  getUserMedia = navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true
-  }).catch(e => alert('getUserMedia() error: ' + e.name))
   socket = io.connect()
-  initRemote = user => {this.getUserMedia.then(() => {
-    this.refs.videoBridge.init(user, this.localStream); console.log(113, this.state.user);})}
-  toggleVideo = () => this.localStream.getVideoTracks()[0].enabled = !this.localStream.getVideoTracks()[0].enabled;
+  initRemote = user => {
+    this.refs.videoBridge.init(user); console.log(113, user);}
   componentDidMount() {
-    this.getUserMedia
-      .then(stream => {
-        this.localStream = stream;
-        console.log(111, 'this.localStream', this.localStream)
-        //this.haveMedia = true;
-        this.refs.localVideo.src = window.URL.createObjectURL(stream);
-      });
     this.props.router.setRouteLeaveHook(this.props.route, () => {
-      this.localStream.getVideoTracks()[0].stop();
+      //this.localStream.getVideoTracks()[0].stop();
       //this.haveMedia = false;
       this.socket.emit('leave');
-      console.log('disconnect', this.socket)
     });
   }
   render(){
@@ -41,13 +21,7 @@ class Room extends React.Component {
     return (
       <div>
         <Auth socket={this.socket} initRemote={this.initRemote} />
-        <div className="media-port">
-          <video ref="localVideo" autoPlay muted></video>
-          <button onClick={this.handleAudio} data-ref="audio">Audio</button>
-          <button onClick={this.toggleVideo} data-ref="video">Video</button>
-          <button onClick={this.handleFullScreen} data-ref="full">Full</button>
-          <VideoBridge ref="videoBridge" getUserMedia={this.getUserMedia} socket={this.socket} />
-        </div>
+        <VideoBridge ref="videoBridge" socket={this.socket} />
         <div>Waiting for someone to join this room:
         	<a href={href}>{href}</a>
         </div>
--- src/modules/VideoBridge.js
+++ src/modules/VideoBridge.js
@@ -4,81 +4,99 @@ export default class VideoBridge extends React.Component {
   constructor(props) {
     super(props);
   }
-  init(role, localStream) {
-    let dc,
-      pc,
-      remoteStream,
-      haveMedia = false,
-      onMessage = message => {
-          console.log('message from peer:', message);
-          if (message.type === 'offer') {
-              // set remote description and answer
-              pc.setRemoteDescription(new RTCSessionDescription(message));
-              pc.createAnswer()
-                .then(setDescription)
-                .then(sendDescription)
-                .catch(handleError); // An error occurred, so handle the failure to connect
-
-          } else if (message.type === 'answer') {
-              // set remote description
-              pc.setRemoteDescription(new RTCSessionDescription(message));
-          } else if (message.type === 'candidate') {
-              // add ice candidate
-              pc.addIceCandidate(
-                  new RTCIceCandidate({
-                      sdpMLineIndex: message.mlineindex,
-                      candidate: message.candidate
-                  })
-              );
-          }
-      },
-      sendData = msg => dc.send(JSON.stringify(msg)),
-      setDescription = offer => pc.setLocalDescription(offer),
-      // send the offer to a server to be forwarded to the other peer
-      sendDescription = () => {console.log(201, 'sendDescription', pc.localDescription);this.props.socket.send(pc.localDescription)},
-      // Set up the data channel message handler
-      setupDataHandlers = () => {
-          dc.onmessage = e => {
-              var msg = JSON.parse(e.data);
-              console.log('received message over data channel:' + msg);
-          };
-          dc.onclose = () => {
-              //this.props.localStream.getVideoTracks()[0].stop();
-              remoteStream.getVideoTracks()[0].stop();
-              //haveMedia = false;
-              role = '';
-              //this.props.setUser('');
-              console.log('The Data Channel is Closed');
-          };
-      },
-      handleError = e => console.log(e),
-      // If RTCPeerConnection is ready and we have local media,
-      // attach media to pc
-      attachMediaIfReady = () => {
-        haveMedia = this.props.haveMedia;
-        console.log('attachMediaIfReady', pc, localStream, /*this.props.user*/);
-        if (pc/* && haveMedia*/) {
-          pc.addStream(localStream);
-          // call if we were the last to connect (to increase
-          // chances that everything is set up properly at both ends)
-          if (role === 'host') {
-              dc = pc.createDataChannel('chat');
-              setupDataHandlers();
-              console.log(200, 'createOffer')
-              pc.createOffer()
-                .then(setDescription)
-                .then(sendDescription)
-                .catch(handleError); // An error occurred, so handle the failure to connect
-          }
-        }
-      };
+  state = {
+      audio: false,
+      video: true,
+      full: false
+  }
+  getUserMedia = navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+  }).catch(e => alert('getUserMedia() error: ' + e.name))
+  componentDidMount() {
     // chrome polyfill for connection between the local device and a remote peer
     window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection;
+    //this.pc = new RTCPeerConnection({iceServers: [{url: 'stun:stun.l.google.com:19302'}]});
+    //this.setPeerConnection();
+    this.getUserMedia
+      .then(stream => {
+        /*this.localStream = stream;
+        this.pc.addStream(stream);
+        console.log(111, 'this.localStream', this.localStream)*/
+        //this.haveMedia = true;
+        this.refs.localVideo.src = window.URL.createObjectURL(stream);
+      });
+    this.props.socket.on('message', this.onMessage);
+  }
+  setPeerConnection() {
+  }
+  onMessage = message => {
+      console.log('message from peer:', message);
+      if (message.type === 'offer') {
+          // set remote description and answer
+          this.pc.setRemoteDescription(new RTCSessionDescription(message));
+          this.pc.createAnswer()
+            .then(this.setDescription)
+            .then(this.sendDescription)
+            .catch(this.handleError); // An error occurred, so handle the failure to connect
+
+      } else if (message.type === 'answer') {
+          // set remote description
+          this.pc.setRemoteDescription(new RTCSessionDescription(message));
+      } else if (message.type === 'candidate') {
+          // add ice candidate
+          this.pc.addIceCandidate(
+              new RTCIceCandidate({
+                  sdpMLineIndex: message.mlineindex,
+                  candidate: message.candidate
+              })
+          );
+      }
+  }
+  sendData(msg) {
+    this.dc.send(JSON.stringify(msg))
+  }
+  // Set up the data channel message handler
+  setupDataHandlers() {
+      this.dc.onmessage = e => {
+          var msg = JSON.parse(e.data);
+          console.log('received message over data channel:' + msg);
+      };
+      this.dc.onclose = () => {
+          //this.props.localStream.getVideoTracks()[0].stop();
+          this.remoteStream.getVideoTracks()[0].stop();
+          //haveMedia = false;
+          //this.role = '';
+          //this.props.setUser('');
+          
+          console.log('The Data Channel is Closed');
+      };
+  }
+  setDescription = offer => this.pc.setLocalDescription(offer)
+  // send the offer to a server to be forwarded to the other peer
+  sendDescription = () => {console.log(201, 'sendDescription', this.pc.localDescription);this.props.socket.send(this.pc.localDescription)}
+  handleError = e => console.log(e)
+  init(role) {
+      // If RTCPeerConnection is ready and we have local media,
+      // attach media to pc
+    // wait for local media to be ready
+    const attachMediaIfReady = () => {
+      this.dc = this.pc.createDataChannel('chat');
+        this.setupDataHandlers();
+        console.log(200, 'createOffer')
+        this.pc.createOffer()
+          .then(this.setDescription)
+          .then(this.sendDescription)
+          .catch(this.handleError); // An error occurred, so handle the failure to connect
+    }
+    console.log('attachMediaIfReady', this.pc, this.localStream, /*this.props.user*/);
     // set up the peer connection
     // this is one of Google's public STUN servers
-    pc = new RTCPeerConnection({iceServers: [{url: 'stun:stun.l.google.com:19302'}]});
+    // make sure your offer/answer role does not change. If user A does a SLD
+    // with type=offer initially, it must do that during  the whole session
+    this.pc = new RTCPeerConnection({iceServers: [{url: 'stun:stun.l.google.com:19302'}]});
     // when our browser gets a candidate, send it to the peer
-    pc.onicecandidate = e => {
+    this.pc.onicecandidate = e => {
         console.log(e, 'onicecandidate');
         if (e.candidate) {
             this.props.socket.send({
@@ -89,30 +107,42 @@ export default class VideoBridge extends React.Component {
         }
     };
     // when the other side added a media stream, show it on screen
-    pc.onaddstream = e => {
+    this.pc.onaddstream = e => {
         console.log('onaddstream', e) 
-        remoteStream = e.stream;
-        this.refs.remoteVideo.src = window.URL.createObjectURL(remoteStream);
+        this.remoteStream = e.stream;
+        this.refs.remoteVideo.src = window.URL.createObjectURL(this.remoteStream);
     };
-    pc.ondatachannel = e => {
+    this.pc.ondatachannel = e => {
         // data channel
-        dc = e.channel;
-        setupDataHandlers();
-        sendData({
+        this.dc = e.channel;
+        this.setupDataHandlers();
+        this.sendData({
           peerMediaStream: {
-            video: localStream.getVideoTracks()[0].enabled/*,
-            audio: this.props.localStream.getAudioTracks()[0].enabled*/
+            video: this.localStream.getVideoTracks()[0].enabled
           }
         });
         //sendData('hello');
     };
-    // wait for local media to be ready
-    this.props.getUserMedia.then(attachMediaIfReady);
-    this.props.socket.on('message', onMessage);
+    this.getUserMedia
+      .then(stream => {
+        this.localStream = stream;
+        this.pc.addStream(stream);
+        // call if we were the last to connect (to increase
+        // chances that everything is set up properly at both ends)
+        if (role === 'host') {
+          this.getUserMedia.then(attachMediaIfReady);
+        }
+      });
+    
   }
+  toggleVideo = () => this.localStream.getVideoTracks()[0].enabled = !this.localStream.getVideoTracks()[0].enabled;
   render(){
     return (
-      <div>
+      <div className="video-bridge">
+        <video ref="localVideo" autoPlay muted></video>
+        <button onClick={this.handleAudio} data-ref="audio">Audio</button>
+        <button onClick={this.toggleVideo} data-ref="video">Video</button>
+        <button onClick={this.handleFullScreen} data-ref="full">Full</button>
         <video ref="remoteVideo" autoPlay></video>
       </div>
     );
