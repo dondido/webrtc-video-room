1b3b0f1672ac0ed871052d27e5558886ddee331f
Dian Dimitrov <dian.dimitrov@kallidus.com>
2016-09-17 17:00:23 +0300

    Popups need styling

--- src/components/MediaBridge.js
+++ src/components/MediaBridge.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ToggleFullScreen from './ToggleFullScreen'
 export default class MediaBridge extends React.Component {
   constructor(props) {
     super(props);
@@ -7,8 +7,7 @@ export default class MediaBridge extends React.Component {
   state = {
     bridge: 'media-bridge',
     audio: true,
-    video: true,
-    full: false
+    video: true
   }
   getUserMedia = navigator.mediaDevices.getUserMedia({
     audio: true,
@@ -130,14 +129,14 @@ export default class MediaBridge extends React.Component {
   }
   toggleVideo = () => this.setState({video: this.localStream.getVideoTracks()[0].enabled = !this.state.video})
   toggleAudio = () => this.setState({audio: this.localStream.getAudioTracks()[0].enabled = !this.state.audio})
-  handleHangup = () => {}
+  handleHangup = () => this.componentWillUnmount()
   render(){
     return (
       <div className={this.state.bridge}>
         <video className="remote-video" ref="remoteVideo" autoPlay></video>
         <video className="local-video" ref="localVideo" autoPlay muted></video>
         <div className="media-controls">
-          <button onClick={this.handleAudio} className={'audio-button-' + this.state.audio}>
+          <button onClick={this.toggleAudio} className={'audio-button-' + this.state.audio}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="svg">
               <path className="on" d="M38 22h-3.4c0 1.49-.31 2.87-.87 4.1l2.46 2.46C37.33 26.61 38 24.38 38 22zm-8.03.33c0-.11.03-.22.03-.33V10c0-3.32-2.69-6-6-6s-6 2.68-6 6v.37l11.97 11.96zM8.55 6L6 8.55l12.02 12.02v1.44c0 3.31 2.67 6 5.98 6 .45 0 .88-.06 1.3-.15l3.32 3.32c-1.43.66-3 1.03-4.62 1.03-5.52 0-10.6-4.2-10.6-10.2H10c0 6.83 5.44 12.47 12 13.44V42h4v-6.56c1.81-.27 3.53-.9 5.08-1.81L39.45 42 42 39.46 8.55 6z" fill="white"></path>
               <path className="off" d="M24 28c3.31 0 5.98-2.69 5.98-6L30 10c0-3.32-2.68-6-6-6-3.31 0-6 2.68-6 6v12c0 3.31 2.69 6 6 6zm10.6-6c0 6-5.07 10.2-10.6 10.2-5.52 0-10.6-4.2-10.6-10.2H10c0 6.83 5.44 12.47 12 13.44V42h4v-6.56c6.56-.97 12-6.61 12-13.44h-3.4z" fill="white"></path>
@@ -149,17 +148,15 @@ export default class MediaBridge extends React.Component {
               <path className="off" d="M40 8H8c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V12c0-2.21-1.79-4-4-4zm-4 24l-8-6.4V32H12V16h16v6.4l8-6.4v16z" fill="white"></path>
             </svg>
           </button>
-          <button onClick={this.handleFullScreen} className={'fullscreen-button-' + this.state.full}>
+          <button onClick={ToggleFullScreen} className="fullscreen-button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="svg">
               <path className="on" d="M10 32h6v6h4V28H10v4zm6-16h-6v4h10V10h-4v6zm12 22h4v-6h6v-4H28v10zm4-22v-6h-4v10h10v-4h-6z" fill="white"></path>
               <path className="off" d="M14 28h-4v10h10v-4h-6v-6zm-4-8h4v-6h6v-4H10v10zm24 14h-6v4h10V28h-4v6zm-6-24v4h6v6h4V10H28z" fill="white"></path>
             </svg>
           </button>
-          <button onClick={this.handleHangup}>
-            <svg id="hangup" className="hidden" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="-10 -10 68 68">
-              <circle cx="24" cy="24" r="34">
-              </circle>
-              <path transform="scale(0.7), translate(11,10)" d="M24 18c-3.21 0-6.3.5-9.2 1.44v6.21c0 .79-.46 1.47-1.12 1.8-1.95.98-3.74 2.23-5.33 3.7-.36.35-.85.57-1.4.57-.55 0-1.05-.22-1.41-.59L.59 26.18c-.37-.37-.59-.87-.59-1.42 0-.55.22-1.05.59-1.42C6.68 17.55 14.93 14 24 14s17.32 3.55 23.41 9.34c.37.36.59.87.59 1.42 0 .55-.22 1.05-.59 1.41l-4.95 4.95c-.36.36-.86.59-1.41.59-.54 0-1.04-.22-1.4-.57-1.59-1.47-3.38-2.72-5.33-3.7-.66-.33-1.12-1.01-1.12-1.8v-6.21C30.3 18.5 27.21 18 24 18z" fill="white"></path>
+          <button onClick={this.handleHangup} className="hangup-button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="svg">
+              <path d="M24 18c-3.21 0-6.3.5-9.2 1.44v6.21c0 .79-.46 1.47-1.12 1.8-1.95.98-3.74 2.23-5.33 3.7-.36.35-.85.57-1.4.57-.55 0-1.05-.22-1.41-.59L.59 26.18c-.37-.37-.59-.87-.59-1.42 0-.55.22-1.05.59-1.42C6.68 17.55 14.93 14 24 14s17.32 3.55 23.41 9.34c.37.36.59.87.59 1.42 0 .55-.22 1.05-.59 1.41l-4.95 4.95c-.36.36-.86.59-1.41.59-.54 0-1.04-.22-1.4-.57-1.59-1.47-3.38-2.72-5.33-3.7-.66-.33-1.12-1.01-1.12-1.8v-6.21C30.3 18.5 27.21 18 24 18z" fill="white"></path>
             </svg>
           </button>
         </div>
